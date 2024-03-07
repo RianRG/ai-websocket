90f2c4f292f65d2e46cc2a0b2f511e4128f4286c
rian <rianrgomes@gmail.com>
2024-03-07 14:16:45 -0300

    fastify setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import fastify from 'fastify';
+import websocket from '@fastify/websocket'
+import cors from '@fastify/cors'
 
 const app = fastify();
+
+app.register(cors, {
+  origin: 'http://localhost:4200',
+  credentials: true
+})
+app.register(websocket)
 
 export { app };
