232acd2703d5fda1d42ec4e83b778ec9317156ef
rian <rianrgomes@gmail.com>
2024-03-08 10:53:54 -0300

    different text styles for user and assitent

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IText } from 'src/app/interfaces/IText';
 import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { WebsocketService } from 'src/app/services/websocket.service';
 })
 export class HomeComponent{
   form!: FormGroup;
-  messages: string[] = [];
+  messages: IText[] = [];
   constructor(
     private fb: FormBuilder,
     private websocket: WebsocketService
@@ -21,12 +22,21 @@ export class HomeComponent{
 
   ngOnInit(): void{
     this.websocket.getMessages().subscribe((data: string) =>{
-      this.messages.push(data)
+      this.messages.push({
+        content: data,
+        sender: 'Assistent'
+      })
+      this.form.enable();
     })
   }
 
   onSubmit(){
+    this.form.disable()
     this.websocket.sendMessage(this.form.value.content);
+    this.messages.push({
+      content: this.form.value.content,
+      sender: 'User'
+    });
     this.form.reset();
   }
 }
