a7247903b3f30d7b8142d92f19d10a5861129c12
rian <rianrgomes@gmail.com>
2024-03-12 12:03:26 -0300

    switch database to mysql

--- backend/prisma/migrations/20240307182430_create_db_structure/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Text" (
-    "id" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "sender" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Text_pkey" PRIMARY KEY ("id")
-);
