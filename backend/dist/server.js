c4c134823af1dd38f423f1b0c29d628d71f9de73
rian <rianrgomes@gmail.com>
2024-03-14 12:04:52 -0300

    update server port

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 app_1.app.listen({
-    port: 5000
+    port: Number(process.env.PORT) || 5000
 }).then(() => console.log('runnin on 5000'));
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
 import { app } from "./app";
 
 app.listen({
-  port: 5000
+  port: Number(process.env.PORT) || 5000
 }).then(() => console.log('runnin on 5000'));
