d31efacf2bf6dd08078a11d3a0511c4621b2f0d1
rian <rianrgomes@gmail.com>
2024-03-14 13:14:33 -0300

    set host for deploy

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 app_1.app.listen({
+    host: '0.0.0.0',
     port: Number(process.env.PORT) || 5000
 }).then(() => console.log('runnin on 5000'));
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 
 app.listen({
+  host: '0.0.0.0',
   port: Number(process.env.PORT) || 5000
 }).then(() => console.log('runnin on 5000'));
