18704db7362693d02d20c57f8d7757172e59f5ba
rian <rianrgomes@gmail.com>
2024-04-08 15:24:02 -0300

    implement mocked client for tests

--- backend/src/tests/get-texts.spec.ts
+++ backend/src/tests/get-texts.spec.ts
@@ -1,14 +1,28 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { PrismaRepository } from '../repositories/prisma-repository'
+import prisma from '../repositories/__mocks__/prisma-repository'
 
 const prismaRepository = new PrismaRepository()
 
+// vi.mock('../repositories/prisma-repository')
+
 describe('text routes', () =>{
   it('should be able to list all texts', async () =>{
+
+    const mockText = {
+      id: '1',
+      content: 'content',
+      sender: 'sender',
+      createdAt: new Date()
+    }
+
+    prisma.text.findMany.mockResolvedValue([mockText])
+
     const texts = await prismaRepository.getTexts()
 
-    console.log(texts)
 
-    expect(200).toEqual(200)
+    console.log(texts[0])
+
+    expect(texts[0].content).toContain('Olá')
   })
 })
