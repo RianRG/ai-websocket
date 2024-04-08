674a3c57973e0254aeea506d84b7e7a242adb7d0
rian <rianrgomes@gmail.com>
2024-04-08 15:56:30 -0300

    complete prisma repository tests with MOCK

--- backend/src/tests/prisma.repository.spec.ts
+++ backend/src/tests/prisma.repository.spec.ts
@@ -4,7 +4,7 @@ import prisma from '../repositories/__mocks__/prisma-repository'
 
 const prismaRepository = new PrismaRepository()
 
-// vi.mock('../repositories/prisma-repository')
+vi.mock('../libs/prisma')
 
 describe('text cases', () =>{
 
@@ -37,5 +37,47 @@ describe('text cases', () =>{
     const text = await prismaRepository.createText(newText)
 
     expect(text.content).toContain(newText.content)
+  })
+
+  it('should be able to create a image request', async () =>{
+    const testImageRequest = { content: 'Id like a photo of a cake'};
+
+    prisma.imageRequest.create.mockResolvedValue({ ...testImageRequest, id: '1', createdAt: new Date() })
+
+    const imageRequest = await prismaRepository.createImageRequest(testImageRequest.content)
+
+    expect(imageRequest.content).toContain(testImageRequest.content)
+  })
+
+  it('should be able to list all images', async () =>{
+    const mockImage = {
+      id: '1',
+      link: 'www.any.com',
+      imageRequestId: '2',
+      createdAt: new Date()
+    }
+
+    prisma.image.findMany.mockResolvedValue([mockImage])
+
+    const images = await prismaRepository.getImages()
+
+
+    expect(images[0].imageRequest).toContain('Uma pessoa')
+  })
+
+  it('should be able to list all imageRequests', async () =>{
+    const mockImageRequest = {
+      id: '1',
+      content: 'gimme a photo of weapons',
+      createdAt: new Date()
+    }
+
+    prisma.imageRequest.findMany.mockResolvedValue([mockImageRequest])
+
+    const imageRequests = await prismaRepository.getImageRequests()
+
+    console.log(imageRequests)
+
+    expect(200).toEqual(200);
   })
 })
